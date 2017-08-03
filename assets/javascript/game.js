4dba1f2a2c6ecde393357d1117f7c4e6e024ad7a
Caden Mayo <cadenmayo@gmail.com>
2017-08-02 22:10:22 -0400

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,13 @@
 var plUnit;
 var enemyUnit;
+var gobbosAr=[];
 var defendUnit;
+var pG=0;
+var plStats;
+var enStats;
+var plIndex;
+var enIndex;
+var sHTMLOut;
 var ga = {
 
 	goblins: {
@@ -28,7 +35,7 @@ var ga = {
 		4:{
 			name: "Gortaxzo",
 			hp: 180,
-			baseATK: Math.floor((Math.random()*2)+1)==1?1:95,
+			baseATK: 2,
 			def: 10,
 			image: "assets/images/Gortaxzo.jpg"
 		}
@@ -36,47 +43,84 @@ var ga = {
 	},
 	loadGoblin: function(){
 		var goblin;
-		var sHTMLOut;
-		//var sHTMLOut = [];
-		//console.log("goodbye");
 		$.each(ga.goblins, function(key, goblin){
 			sHTMLOut = ("<div id='gob_" + key + "' class='gBox'><span id = 'gobName_"+key+"' class='gName'>" + goblin.name + "</span><br><img class='gImage' id='gobImg_"+key+"' src='" + goblin.image + "'/><br><span id ='gobHP_"+key+"' class='gHP'>" + goblin.hp + "</span></div>");
-			//sHTMLOut.push();
-			//sHTMLOut.push("");
-			//sHTMLOut.push(");
-			//sHTMLOut.push(");
-			//console.log("Hello");
-
 			$("#player").append(sHTMLOut);
 		});
 	},
-	enemyGoblin: function(key){
-		$.each(ga.goblins, function(){
-			$("#enemy").append("gob_"+key);			
-		} )
+	enemyGoblin: function(posit){
+		if(pG===0){
+			var picked = posit;
+			var divfoc= document.getElementById("gob_"+posit);
+			plUnit = divfoc;
+			var goblin;
+			posit=1;
+			$.each(ga.goblins, function(key,goblin){
+				if(posit != picked){
+					divfoc= document.getElementById("gob_"+posit);
+					$("#enemy").append(divfoc);	
+					posit++;	
+				}
+				else{
+					posit++;
+				}
+
+				})
+			picked--;
+			console.log(gobbosAr[picked]);
+			plStats = gobbosAr[picked];
+			//return ga.goblins.picked;	
+			};
+		},
+	defendGoblin: function(posit){
+		if(pG===1)
+		{
+		var divfoc = document.getElementById("gob_"+posit);
+		$("#defender").append(divfoc);
+		posit--;
+		enStats = gobbosAr[posit];
+		return;
+		}
 	}
-};
 
+}
 
+function battle(obj1, obj2,posit1,posit2){
+	obj2.hp = (obj2.hp) -(obj1.baseATK);
+	obj1.baseATK = obj1.baseATK + obj1.baseATK;
+	var hpSpan = document.getElementById("gobHP_"+posit1);
+	$(hpSpan).text(obj1.hp);
+	if(obj2.hp>0){
+		obj1.hp = obj1.hp - obj2.def; 
+		hpSpan = document.getElementById("gobHP_"+posit2);
+		$(hpSpan).text(obj1.hp);
+	}
+	else{
+		$("#defender").empty();
+		pG=1;
+		return;
+	}
+	//console.log(obj1);
+	//console.log(obj2);
+}
 
 $(document).ready(function(){
-			//$("#player").append(ga.loadGoblin());
 	ga.loadGoblin();
-	$(".gBox").on("click",function(event) {
-    plUnit = (event.target.id.slice(-1));
-    console.log(plUnit);
-    //if(plUnit == 'gob_1'||plUnit=='gobImg_1'){
-      // console.log("hello");
-       //stop();
-    //}
-   	//});
-   	switch(plUnit){
-   		case "1": ga.enemyGoblin(,plUnit);
-   			break;
-   		case "2":
-   		case "3":
-   		case "4":
-   //		break;
-	}
-  	});
+	gobbosAr=[ga.goblins[1],ga.goblins[2],ga.goblins[3],ga.goblins[4]];
+	$("#player .gBox").on("click",function(event) {
+    var clicked = (event.target.id.slice(-1));
+    console.log(clicked);
+    if(pG===0){
+    	ga.enemyGoblin(clicked);
+    	plIndex=clicked;
+    }
+    else if(pG===1){
+        ga.defendGoblin(clicked);
+        enIndex=clicked;
+    }
+    else{
+    	console.log("oop");
+    }
+    pG++;
+   });
 });
